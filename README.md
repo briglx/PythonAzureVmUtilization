31d5151c24d47ad713b4b0f8acd7b8c6b7cb2fd7
Brig Lamoreaux <bklamoreaux@gmail.com>
2020-04-24 15:42:16 -0700

    Add subscription and metric check.

--- README.md
+++ README.md
@@ -1,6 +1,47 @@
 
 Example on using Docker to fetch metrics form Azure VM
 
+# Notes
+
+Should call `client.metric_definitions.list` to get the exact metric details for each metric. See https://docs.microsoft.com/en-us/rest/api/monitor/metricdefinitions/list
+
+Interesting Metrics for VMs
+
+- Percentage CPU. The percentage of allocated compute units that are currently in use by the Virtual Machine(s)
+- Disk Read Bytes
+- Disk Write Bytes
+- Network In Total. The number of bytes received on all network interfaces by the Virtual Machine(s) (Incoming Traffic)
+- Network Out Total
+
+
+Most metrics allow time grain to be any of the following
+
+```python
+class TimeGrain(str, Enum):
+
+    pt1_m = "PT1M"
+    pt5_m = "PT5M"
+    pt15_m = "PT15M"
+    pt30_m = "PT30M"
+    pt1_h = "PT1H"
+    pt6_h = "PT6H"
+    pt12_h = "PT12H"
+    pt1_d = "PT1D"
+```
+
+Most metrics allow the following Aggregation types
+
+```python
+class AggregationType(str, Enum):
+
+    none = "None"
+    average = "Average"
+    count = "Count"
+    minimum = "Minimum"
+    maximum = "Maximum"
+    total = "Total"
+```
+
 # Build and Run
 
 
@@ -14,7 +55,11 @@ Run the image
 docker run --rm -it --env-file local.env  azurepythonexample:latest
 ```
 
+
+
 # References
 - Azure for Python Developers https://docs.microsoft.com/en-us/azure/python/?view=azure-python
 - List all subscriptions https://docs.microsoft.com/en-us/python/api/azure-mgmt-subscription/azure.mgmt.subscription.operations.subscriptionsoperations?view=azure-python#list-custom-headers-none--raw-false----operation-config-
 - Example Fetch Metrics https://stackoverflow.com/questions/54327418/get-cpu-utilization-of-virtual-machines-in-azure-using-python-sdk
+- MetricsOperation Class https://docs.microsoft.com/en-us/python/api/azure-mgmt-monitor/azure.mgmt.monitor.v2018_01_01.operations.metricsoperations?view=azure-python
+- Definitions of Metrics https://docs.microsoft.com/en-us/rest/api/monitor/metricdefinitions/list#definitions
--- main.py
+++ main.py
@@ -2,28 +2,36 @@
 import logging
 import json
 import os
-import datetime
+import csv
+from datetime import datetime, timedelta
 from azure.common.client_factory import get_client_from_json_dict
-from azure.mgmt.resource import ResourceManagementClient
+from azure.mgmt.subscription import SubscriptionClient
 from azure.mgmt.compute import ComputeManagementClient
 from azure.mgmt.monitor import MonitorManagementClient
+import numpy as np
 
 
 _LOGGER = logging.getLogger(__name__)
-_LOGGER.setLevel(logging.DEBUG)
-ch = logging.StreamHandler()
-formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
-ch.setFormatter(formatter)
-_LOGGER.addHandler(ch)
 
 
-def get_management_client(azure_auth):
+def configure_logger():
+    """Configure logger"""
+    _LOGGER.setLevel(logging.INFO)
+    handler = logging.StreamHandler()
+    formatter = logging.Formatter(
+        "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+    )
+    handler.setFormatter(formatter)
+    _LOGGER.addHandler(handler)
+
+
+def get_management_client(azure_auth, client_class):
     """Get Container Client."""
     client = None
     if azure_auth is not None:
         _LOGGER.info("Authenticating Azure using credentials")
         auth_config_dict = json.loads(azure_auth)
-        client = get_client_from_json_dict(MonitorManagementClient, auth_config_dict)
+        client = get_client_from_json_dict(client_class, auth_config_dict)
     else:
         _LOGGER.error(
             "\nFailed to authenticate to Azure. Have you set the"
@@ -32,32 +40,167 @@ def get_management_client(azure_auth):
     return client
 
 
-def main():
-    azure_auth = os.environ.get("AZURE_AUTH")
-    resource_id = os.environ.get("RESOURCE_ID")
+def get_metric_definitions(client, resource_id):
+    """Get details about the metrics"""
+    metric_definitions = client.metric_definitions.list(
+        resource_id, metricnamespace="Microsoft.Compute/virtualMachines", raw=True
+    )
+    for metric_definition in metric_definitions:
+        _LOGGER.info(metric_definition.name)
+        for supported_aggregation_type in metric_definition.supported_aggregation_types:
+            _LOGGER.info(supported_aggregation_type)
+        for metric_availability in metric_definition.metric_availabilities:
+            _LOGGER.info(metric_availability)
 
-    client = get_management_client(azure_auth)
 
-    today  = datetime.datetime.now().date()
-    yesterday = today - datetime.timedelta(days=1)
+def get_mean(array):
+    """Get mean from passed array"""
+    np_array = np.asarray(array)
+    if np_array.any():
+        return np_array.mean()
+    return None
 
-    metrics_data = client.metrics.list(
+
+def get_max(array):
+    """Get mean from passed array"""
+    np_array = np.asarray(array)
+    if np_array.any():
+        return np_array.max()
+    return None
+
+
+def get_min(array):
+    """Get mean from passed array"""
+    np_array = np.asarray(array)
+    if np_array.any():
+        return np_array.min()
+    return None
+
+
+def get_vm_metrics(monitor_client, resource_id):
+    """Get metrics for the given vm. Returns row of cpu, disk, network activity"""
+
+    today = datetime.utcnow().date()
+    last_week = today - timedelta(days=7)
+
+    metrics_data = monitor_client.metrics.list(
         resource_id,
-        timespan="{}/{}".format(yesterday, today),
-        interval='PT1H',
-        metric='Percentage CPU',
-        aggregation='Total'
+        timespan="{}/{}".format(last_week, today),
+        interval="PT12H",
+        metricnames="Percentage CPU,Disk Read Bytes,Disk Write Bytes,Network In Total,Network Out Total",
+        aggregation="Minimum,Average,Maximum",
     )
 
+    row = {}
+    ave_cpu = []
+    min_cpu = []
+    max_cpu = []
+    ave_disk_read = []
+    ave_disk_write = []
+    ave_network_in = []
+    ave_network_out = []
+
     for item in metrics_data.value:
-        # azure.mgmt.monitor.models.Metric
-        _LOGGER.info("{} ({})".format(item.name.localized_value, item.unit.name))
-        for timeserie in item.timeseries:
-            for data in timeserie.data:
-                # azure.mgmt.monitor.models.MetricData
-                _LOGGER.info("{}: {}".format(data.time_stamp, data.total))
+        if item.name.value == "Percentage CPU":
+            for timeserie in item.timeseries:
+                for data in timeserie.data:
+                    if data.average:
+                        ave_cpu.append(data.average)
+                    if data.minimum:
+                        min_cpu.append(data.minimum)
+                    if data.maximum:
+                        max_cpu.append(data.maximum)
+
+        if item.name.value == "Disk Read Bytes":
+            for timeserie in item.timeseries:
+                for data in timeserie.data:
+                    if data.average:
+                        ave_disk_read.append(data.average)
+
+        if item.name.value == "Disk Write Bytes":
+            for timeserie in item.timeseries:
+                for data in timeserie.data:
+                    if data.average:
+                        ave_disk_write.append(data.average)
+
+        if item.name.value == "Network In Total":
+            for timeserie in item.timeseries:
+                for data in timeserie.data:
+                    if data.average:
+                        ave_network_in.append(data.average)
+
+        if item.name.value == "Network Out Total":
+            for timeserie in item.timeseries:
+                for data in timeserie.data:
+                    if data.average:
+                        ave_network_out.append(data.average)
+
+    row = (
+        get_mean(ave_cpu),
+        get_min(min_cpu),
+        get_max(max_cpu),
+        get_mean(ave_disk_read),
+        get_mean(ave_disk_write),
+        get_mean(ave_network_in),
+        get_mean(ave_network_out),
+    )
+
+    return row
+
+
+def save_to_file(header, items):
+    """Save items to csv file"""
+
+    dte = datetime.utcnow()
+    local_filename = "vm-utilization-%s.csv" % (dte.isoformat())
+    local_filename = local_filename.replace(":", "-")
+    with open(local_filename, "w", newline="") as utilization_report:
+        writer = csv.writer(utilization_report, delimiter=",")
+
+        writer.writerows([header])
+
+        for row in items:
+
+            writer.writerows([row])
+
+
+def main():
+    """Main function to get metrics"""
+    azure_auth = os.environ.get("AZURE_AUTH")
+
+    # Get Clients
+    subscription_client = get_management_client(azure_auth, SubscriptionClient)
+    compute_client = get_management_client(azure_auth, ComputeManagementClient)
+    monitor_client = get_management_client(azure_auth, MonitorManagementClient)
+
+    rows = []
+
+    # Iterate each subscription
+    for subscription in subscription_client.subscriptions.list():
+        _LOGGER.info(subscription.display_name, subscription.subscription_id)
+
+        for virtual_machine in compute_client.virtual_machines.list_all():
+            _LOGGER.info("%s %s", virtual_machine.name, virtual_machine.id)
+            row = get_vm_metrics(monitor_client, virtual_machine.id)
+            rows.append((subscription.display_name, virtual_machine.id, *row))
+
+    header = list(
+        [
+            "subscription_name",
+            "resource_id",
+            "cpu_mean",
+            "cpu_min",
+            "cpu_max",
+            "disk_read_mean",
+            "disk_write_mean",
+            "network_in_mean",
+            "network_out_mean",
+        ]
+    )
+    save_to_file(header, rows)
 
 
 if __name__ == "__main__":
+    configure_logger()
     _LOGGER.info("starting")
     main()
